af70bc605548d65efab769710c65e4547a505e9b
Cindi Morales <redheadcindy123@gmail.com>
2019-05-15 21:43:23 +0000

    hello

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // JavaScript File
+$.ajax({
+    url:"https://api.soundcloud.com/tracks?q=beyonce&client_id=5aa8e389ba4e24b6106af5159ab3e344",
+    method:"GET",
+success:function(response) {
+    console.log(response)
+},    
+});
+var Sad_Songs=
+["We Don't Talk Anymore by Charlie Puth (feat. Selene Gomez)","",
+"Stigma by BTS",
+"No One by Alicia Keys",
+"",
+"",
+""];
