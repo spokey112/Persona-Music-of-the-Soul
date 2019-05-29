ce7a994b08399b998f4196d62b19c0693a71570a
Cindi Morales <redheadcindy123@gmail.com>
2019-05-29 21:09:45 +0000

    message

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 // JavaScript File
+
+var artists=
+["",
+"AliciaKeys",
+"J.Cole",
+"Beyonce",
+"ChildishGambino",
+"Beyondthescene",
+""];
+
+var artistName= artists[3];
+
 $.ajax({
-    url:"https://api.soundcloud.com/tracks?q=beyonce&client_id=5aa8e389ba4e24b6106af5159ab3e344",
+    url:"https://api.soundcloud.com/tracks?q="+artistName+"&client_id=5aa8e389ba4e24b6106af5159ab3e344",
     method:"GET",
 success:function(response) {
     console.log(response)
 },    
 });
-var Sad_Songs=
-["We Don't Talk Anymore by Charlie Puth (feat. Selene Gomez)","",
-"Stigma by BTS",
-"No One by Alicia Keys",
-"",
-"",
-""];
