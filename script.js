275f0698edba2dde52f1d0cec00b56fd713778ac
Cindi Morales <redheadcindy123@gmail.com>
2019-05-29 21:47:31 +0000

    idk

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // JavaScript File
+/* global $ */
 
-var artists=
-["",
-"AliciaKeys",
-"J.Cole",
-"Beyonce",
-"ChildishGambino",
-"Beyondthescene",
-""];
+var artists = [
+    "AliciaKeys",
+    "J.Cole",
+    "Beyonce",
+    "ChildishGambino",
+    "Beyondthescene"
+];
 
-var artistName= artists[3];
+var artistName = artists[Math.floor(Math.random() * artists.length)];
 
 $.ajax({
-    url:"https://api.soundcloud.com/tracks?q="+artistName+"&client_id=5aa8e389ba4e24b6106af5159ab3e344",
-    method:"GET",
-success:function(response) {
-    console.log(response)
-},    
+    url: "https://api.soundcloud.com/tracks?q=" + artistName + "&client_id=5aa8e389ba4e24b6106af5159ab3e344",
+    method: "GET",
+    success: function(response) {
+        console.log(response)
+    },
 });
